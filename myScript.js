936cea499ed6a4bae7f02ba77b47c873f99a90ae
NarmeenShah <narmeenshah02@gmail.com>
2018-08-23 21:02:34 +0500

    added form with print on submit

---
+++ myScript.js
@@ -0,0 +1,9 @@
+
+	function myfunc(e)
+	{
+	var x=document.getElementById("opt1").value;
+	var y=document.getElementById("opt2").value;
+	var z=document.getElementById("opt3").value;
+	var i=document.getElementById("opt4").value;
+	console.log("value1:"+x+"value2:"+y+"value3:"+z+"value4:"+i);
+	}
