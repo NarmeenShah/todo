62634d5aec5b6d0f636bfbb6886b1d6b052db195
NarmeenShah <narmeenshah02@gmail.com>
2018-08-23 21:18:30 +0500

    changed radio to select and changed variable names in js

--- myScript.js
+++ myScript.js
@@ -1,9 +1,10 @@
 
 	function myfunc(e)
 	{
-	var x=document.getElementById("opt1").value;
-	var y=document.getElementById("opt2").value;
-	var z=document.getElementById("opt3").value;
-	var i=document.getElementById("opt4").value;
-	console.log("value1:"+x+"value2:"+y+"value3:"+z+"value4:"+i);
+		e.preventDefault();
+	var title=document.getElementById("title").value;
+	var type=document.getElementById("type").value;
+	var description=document.getElementById("description").value;
+	console.log(`**naya task** title: ${title} , type: ${type}, description: ${description}`);
 	}
+	
