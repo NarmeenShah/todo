08cfdb919346fe241c1483c39f95bfae43bcbfa4
NarmeenShah <narmeenshah02@gmail.com>
2018-08-27 21:38:34 +0500

    Table Created

--- myScript.js
+++ myScript.js
@@ -4,6 +4,21 @@
 	var title=document.getElementById("title").value;
 	var type=document.getElementById("type").value;
 	var description=document.getElementById("description").value;
-	console.log(`**naya task** title: ${title} , type: ${type}, description: ${description}`);
-	}
-	
+	console.log(`title: ${title} , type: ${type}, description: ${description}`);
+            
+    		var tr=document.createElement("tr");
+		    
+		    var td1=tr.insertCell();		    
+		    td1.appendChild(document.createTextNode(title));
+		    tr.appendChild(td1);
+		    
+		    var td2=tr.insertCell();
+		    td2.appendChild(document.createTextNode(type));
+		    tr.appendChild(td2);
+		    
+		    var td3=tr.insertCell();
+		    td3.appendChild(document.createTextNode(description));
+		    tr.appendChild(td3);		    
+		    
+		    document.getElementById("tbody").appendChild(tr);		  
+    }
